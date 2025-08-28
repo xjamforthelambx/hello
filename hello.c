257a670599a0489084c6bd5a91976fa186a4846c
William McCarthy <wmccarthy7@huskers.unl.edu>
2025-08-28 17:20:36 +0000

    Thu, Aug 28, 2025, 12:20 PM -05:00

---
+++ hello.c
@@ -0,0 +1,16 @@
+/**
+ * Author: William
+ *         wmccarthy7@unl.edu
+ * Date: 2025/08/26
+ *
+ * A simple hello world program in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("William McCarthy\n");
+  return 0;
+}
